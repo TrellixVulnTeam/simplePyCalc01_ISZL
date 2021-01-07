b12d6deb04a75ad28f693c0b72531de67fdeb2d7
paul <pxl34232@gmail.com>
2021-01-06 16:05:06 -0800

    basic functionality

--- simplePyCalc01/main.py
+++ simplePyCalc01/main.py
@@ -1,6 +1,29 @@
-#addition 
+#addition
+def add(input1, input2):
+    result = input1 + input2
+    print(input1, " + ", input2 , " = ")
+    print(result)
 
+#subtraction
+def subtract(input1, input2):
+    result1 = input1 - input2
+    print(input1 , " - " , input2 , " = ")
+    print(result1)
 
+#multiply
+def multiply(input1, input2):
+    result2 = input1 * input2
+    print(input1 , " * " , input2 , " = ")
+    print(result2)
+
+# division
+def divide(input1, input2):
+    if input2 == 0:
+        print("numbers can 'NOT' be divided by ZERO")
+    else:
+        result3= input1 / input2
+        print(input1 , " / " , input2 , " = ")
+        print(result3)
 
 def main():
     print("""
@@ -12,31 +35,37 @@ def main():
     Please enter two numbers (they can be integers or floats)
     """)
     while True:
-        input1 = float(input("> ").strip())
-        input2 = float(input("> ").strip())
+        try:
+            input1 = float(input("> ").strip())
+            input2 = float(input("> ").strip())
+            break
+        except ValueError:
+            print("Invalid Entry, not a Float or Integer... Try Again")
 
     print("""
          Now do you want to 
-         A) add
-         S) substract
-         M) multiply
-         D) divide
+         A) Add
+         S) Substract
+         M) Multiply
+         D) Divide
          the numbers?
          """)
 
     while True:
       input3 = str(input("> ").strip().lower())
       if input3 == "a":
-           add(input1, input2)
+        add(input1, input2)
+        break
+      elif input3 == "s":
+        subtract(input1, input2)
+        break
+      elif input3 == "m":
+        multiply(input1, input2)
+        break
+      elif input3 == "d":
+        divide(input1, input2)
+        break
+      else:
+        print("Not a valid entry")
 
-        elif input3 == "s":
-            subtract(input1, input2)
-
-        elif input3 == "m":
-            multiply(input1, input2)
-
-        elif input3 == "d":
-            divide(input1, input2)
-
-        else:
-            print("Not a valid entry")
+main()
